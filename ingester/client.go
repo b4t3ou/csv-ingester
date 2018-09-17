51218be175603307195c4232ac2b37f0f7a4eaf8
Kovács Zoltán <zoltan.smith@gmail.com>
2018-09-17 23:53:04 +0100

    added channel to client

--- ingester/client.go
+++ ingester/client.go
@@ -17,14 +17,18 @@ type Client struct {
 	storageClient pb.StorageClient
 	reader        ReaderInterface
 	items         []*pb.Item
+	dataChannel   chan *pb.Item
+	doneChannel   chan struct{}
 }
 
 // NewClient returning with a new client
 func NewClient(cfg *config.Config, reader ReaderInterface) (*Client, error) {
 	c := &Client{
-		config: cfg,
-		reader: reader,
-		items:  []*pb.Item{},
+		config:      cfg,
+		reader:      reader,
+		items:       []*pb.Item{},
+		dataChannel: make(chan *pb.Item),
+		doneChannel: make(chan struct{}),
 	}
 
 	conn, err := grpc.Dial(fmt.Sprintf("%s:%s", cfg.ServerHost, cfg.Port), grpc.WithInsecure())
@@ -39,30 +43,48 @@ func NewClient(cfg *config.Config, reader ReaderInterface) (*Client, error) {
 
 // Run running the client and the data reader
 func (c *Client) Run() {
+	go c.readDataChan()
+
 	c.reader.Receive(c.callback)
-	c.send()
+	close(c.dataChannel)
+
+	<-c.doneChannel
+}
+
+func (c *Client) readDataChan() {
+	var data []*pb.Item
+
+	for {
+		record, open := <-c.dataChannel
+		if !open {
+			c.send(data)
+			c.doneChannel <- struct{}{}
+		}
+
+		data = append(data, record)
+
+		if len(data) == 10 {
+			c.send(data)
+			data = []*pb.Item{}
+		}
+	}
 }
 
 func (c *Client) callback(record Record) {
-	c.items = append(c.items, &pb.Item{
+	c.dataChannel <- &pb.Item{
 		Id:           record.ID,
 		Name:         record.Name,
 		Email:        record.Email,
 		MobileNumber: record.MobileNumber,
-	})
-
-	if len(c.items) == 10 {
-		c.send()
-		c.items = []*pb.Item{}
 	}
 }
 
-func (c *Client) send() error {
+func (c *Client) send(data []*pb.Item) error {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
 	defer cancel()
 
 	_, err := c.storageClient.Save(ctx, &pb.StorageRequest{
-		Items: c.items,
+		Items: data,
 	})
 
 	return err
